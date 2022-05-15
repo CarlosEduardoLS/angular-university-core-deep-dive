45875a516cfba8fc37aaf15fe707eef1b6c99fa3
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-05-15 20:41:16 -0300

    FEAT: Using OnPush change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { COURSES } from '../db-data';
 import { CoursesService } from '../services/courses.service';
 import { Course } from './model/course';
 
@@ -8,24 +7,18 @@ import { Course } from './model/course';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
   constructor(private readonly coursesService: CoursesService) {}
 
-  courses = COURSES;
+  courses$: Observable<Course[]>;
 
-  ngOnInit(): void {}
-
-  onEditCourse() {
-    // Didn't work becouse OnPush change detection
-    // this.courses[0].description = 'new value!';
-
-    const course = this.courses[0];
-
-    const newCourse = { ...course, description: 'New Value' };
-
-    this.courses[0] = newCourse;
+  ngOnInit(): void {
+    this.courses$ = this.coursesService.getCourses();
   }
+
+  onEditCourse() {}
 
   updateCourse(course: Course) {
     this.coursesService
